2d606cbeed4d8c167c2f2731883eaf2eba0e5d0e
khoinguyen26 <khoinguyen261101@gmail.com>
2025-05-24 21:53:53 +0700

    update i3 config

--- i3/set_mirror_display.sh
+++ i3/set_mirror_display.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xrandr --output HDMI-0 --mode 1920x1080 --same-as DP-4
-xrandr --output DP-4 --brightness 0.7
+xrandr --output HDMI-0 --same-as DP-4
+xrandr --output DP-4 --brightness 0.8
